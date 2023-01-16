8b7e0728aa30f343ec2c2ad1a47e704d9376928d
Haoqi Zhang <zhang001221@gmail.com>
2023-01-16 17:00:12 -0500

    change for it

--- amplifyapp/src/CreateVisitor.js
+++ amplifyapp/src/CreateVisitor.js
@@ -1,28 +1,12 @@
-import { useState } from "react";
-import { useHistory } from 'react-router-dom';
 
 const Visitor = () => {
 
-    const [name, setName] = useState('');
-    const [body, setBody] = useState('');
-    const [rate, setRate] = useState('Love it');
-    const [job, setJob] = useState('Other');
-
-    const history = useHistory();
-    
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const visitor = { name, body, rate, job };
-        console.log(visitor);
-        console.log("added");
-    }
 
     return (
         <div className="make-visitor">
             <h2>You don't have to, but...</h2>
             <p>Wish you can leave some infomation about you!</p>
-            <h1>Thanks for your time, but this is not done yet.</h1>
+            <h1>Thanks for your time, but this is not done yet</h1>
         </div>
       );
 }
