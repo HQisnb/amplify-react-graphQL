d2ad24cad45165b65d1640e6020af7278bc3444b
Haoqi Zhang <zhang001221@gmail.com>
2023-01-16 16:27:59 -0500

    change for it

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -1,14 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Self from './myself';
+import Carrer from './carrer';
+import NotFound from './NotFound';
+import Visitor from './CreateVisitor';
+import Education from './Education';
+
 
 function App() {
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <Router>
+
+      <div className="App">
+       <Navbar />
+    
+      <div className="content-direction">
+
+        <Switch>
+
+          <Route exact path="/">
+            <Home />    
+          </Route>
+          <Route exact path="/myself">
+            <Self />
+          </Route>
+          <Route exact path="/carrer">
+            <Carrer />
+          </Route>
+          <Route exact path="/carrer/education">
+            <Education />
+          </Route>
+          <Route exact path="/register">
+            <Visitor />
+          </Route>
+
+          <Route path="*">
+            <NotFound />
+          </Route>
+
+        </Switch>
+
+      </div>
+
+      </div>
+    
+    </Router>
+    
   );
 }
 
